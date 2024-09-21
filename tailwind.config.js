2b6e8fe1bbfbf44d5ee287e10693d67f827bfb41
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-09-21 21:50:21 +0800

    Added Files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'rich-black': '#0D1821',
+        'oxford-blue': '#1B263B',
+        'yimin-blue': '#415A77', 
+        'silver-lake-blue': '#778DA9',
+        'platinum': '#E0E1DD',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat, sans-serif'], // A modern sans-serif font
+        'raleway': ['Raleway, sans-serif'], // A stylish font for headings
+      },
+    },
+  },
+  plugins: [],
+}
+
