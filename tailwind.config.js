0f1f22da11cf2dddf065283891c408b07826a83d
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2025-05-06 01:43:45 +0800

    Update the Portfolio Iinformation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./pages/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     screens: {
-      'tablet': '768px',
-      'md': '1110px',
-      
+      tablet: "768px",
+      md: "1110px",
     },
     extend: {
       colors: {
-        'rich-black': '#0D1821',
-        'oxford-blue': '#1B263B',
-        'yimin-blue': '#415A77', 
-        'silver-lake-blue': '#778DA9',
-        'platinum': '#E0E1DD',
+        "rich-black": "#0D1821",
+        "oxford-blue": "#1B263B",
+        "yimin-blue": "#415A77",
+        "silver-lake-blue": "#778DA9",
+        platinum: "#E0E1DD",
       },
       fontFamily: {
-        'montserrat': ['Montserrat, sans-serif'], // A modern sans-serif font
-        'raleway': ['Raleway, sans-serif'], // A stylish font for headings
+        montserrat: ["Montserrat, sans-serif"], // A modern sans-serif font
+        raleway: ["Raleway, sans-serif"], // A stylish font for headings
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
